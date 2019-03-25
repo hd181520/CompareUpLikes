780c2f3f36d7a9b1d8bc63f8fa983f54f0332847
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-03-25 22:05:19 +0900

    INIT

---
+++ README.md
@@ -0,0 +1,5 @@
+# CompareUpLikes
+Compare likes about Facebook pages
+
+![](cover.png)
+> [CIVAR](https://www.facebook.com/CIVAR18) vs [UNICON](https://www.facebook.com/UNIverse-CONquest-316272942366152)
---
+++ compare_up_likes.py
@@ -0,0 +1,31 @@
+import re, os.path
+from selenium import webdriver
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def compare_likes(URL, log=False):
+  options = webdriver.ChromeOptions()
+  options.add_argument('headless')
+  options.add_argument('window-size=800x500')
+  options.add_argument("disable-gpu")
+
+  driver_path = os.path.join(CURRENT_DIR, './chromedriver')
+  driver = webdriver.Chrome(driver_path, chrome_options=options)
+
+  likes = []
+  for idx, url in enumerate(URL):
+    driver.get(url)
+    driver.implicitly_wait(10)
+    temp = driver.find_element_by_id('PagesLikesCountDOMID').text
+    if log: 
+      print('[*] ', f'"{url}"', temp)
+    likes.append(temp)
+
+  return (' VS '.join(likes))
+
+if __name__ == '__main__':
+  URL = [
+    'https://www.facebook.com/CIVAR18',
+    'https://www.facebook.com/UNIverse-CONquest-316272942366152'
+  ]
+  print(compare_likes(URL, log=True))
