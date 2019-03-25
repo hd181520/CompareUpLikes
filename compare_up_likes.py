a589ac6b4c9716dc5ef9b263afd660a0b05f8d4a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-03-26 01:14:11 +0900

    refactor: fix style with pylint, autopep8

--- compare_up_likes.py
+++ compare_up_likes.py
@@ -1,31 +1,33 @@
-import re, os.path
+import os.path
 from selenium import webdriver
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def compare_likes(URL, log=False):
-  options = webdriver.ChromeOptions()
-  options.add_argument('headless')
-  options.add_argument('window-size=800x500')
-  options.add_argument("disable-gpu")
 
-  driver_path = os.path.join(CURRENT_DIR, './chromedriver')
-  driver = webdriver.Chrome(driver_path, chrome_options=options)
+def compare_likes(urls, log=False):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=800x500')
+    options.add_argument("disable-gpu")
 
-  likes = []
-  for idx, url in enumerate(URL):
-    driver.get(url)
-    driver.implicitly_wait(10)
-    temp = driver.find_element_by_id('PagesLikesCountDOMID').text
-    if log: 
-      print('[*] ', f'"{url}"', temp)
-    likes.append(temp)
+    driver_path = os.path.join(CURRENT_DIR, './chromedriver')
+    driver = webdriver.Chrome(driver_path, chrome_options=options)
 
-  return (' VS '.join(likes))
+    likes = []
+    for url in urls:
+        driver.get(url)
+        driver.implicitly_wait(10)
+        temp = driver.find_element_by_id('PagesLikesCountDOMID').text
+        if log:
+            print('[*] ', f'"{url}"', temp)
+        likes.append(temp)
+
+    return ' VS '.join(likes)
+
 
 if __name__ == '__main__':
-  URL = [
-    'https://www.facebook.com/CIVAR18',
-    'https://www.facebook.com/UNIverse-CONquest-316272942366152'
-  ]
-  print(compare_likes(URL, log=True))
+    LINKS = [
+        'https://www.facebook.com/CIVAR18',
+        'https://www.facebook.com/UNIverse-CONquest-316272942366152'
+    ]
+    print(compare_likes(LINKS, log=True))
